0ca4fc467b71545bc554ab3a3bde800fe598778a
unknown <kalpitraorane47@gmail.com>
2023-08-18 18:51:37 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 import React from "react";
-import  ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
 // const heading  = React.createElement("h1", {}, "Hello World from Rao");
 //     const root = ReactDOM.createRoot(document.getElementById("root"))
 
 //     root.render(heading)
 
-
 // Create this and render on the page.
-{/* <div id = "parent">
+{
+  /* <div id = "parent">
     <div id = "child">
         <h1></h1>
     </div>
-</div> */}
+</div> */
+}
 
 // const parent = React.createElement("div", {id:"parent"},React.createElement("div", {id:"child"},React.createElement("h1", {}, "I am An h2 Tag")))
 
@@ -20,16 +21,11 @@ import  ReactDOM  from "react-dom/client";
 
 // child.render(parent)
 
-
-
 //React element
+const jsxheading =( <h1 className="head">USing JSx</h1>);
 
 
-const jsxheading = (
-<h1 className="head">USing JSx</h1>
-)
 
-const child = ReactDOM.createRoot(document.getElementById("root"))
+const child = ReactDOM.createRoot(document.getElementById("root"));
 
-child.render(jsxheading)
-
+child.render(jsxheading);
