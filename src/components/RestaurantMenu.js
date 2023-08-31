7d7a3fc09dd68317124d01740c0ab3a1855e3455
unknown <kalpitraorane47@gmail.com>
2023-08-31 19:05:47 +0530

    Initial commit

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -2,24 +2,27 @@ import React, { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
 import { MENU_API } from "../utils/constants";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
-  const [resInfo, setResInfo] = useState(null);
-  console.log("resInfo", resInfo);
+  // const [resInfo, setResInfo] = useState(null);
+  // console.log("resInfo", resInfo);
 
   const {resId} = useParams();
 
-  useEffect(() => {
-    fetchMenu();
-  }, []);
+  // useEffect(() => {
+  //   fetchMenu();
+  // }, []);
 
-  const fetchMenu = async () => {
-    const data = await fetch(
-      MENU_API + resId
-    );
-    const json = await data.json();
-    setResInfo(json.data);
-  };
+  // const fetchMenu = async () => {
+  //   const data = await fetch(
+  //     MENU_API + resId
+  //   );
+  //   const json = await data.json();
+  //   setResInfo(json.data);
+  // };
+
+  const resInfo = useRestaurantMenu(resId);
 
   if (!resInfo) {
     // You can render a loading state here if needed
